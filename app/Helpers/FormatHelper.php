40148f9ec038573ffc872146369fe8e3593f272e
gilbertozioma <gilbertozioma0@gmail.com>
2025-04-24 08:04:35 +0100

    added FormatHelper class with formatBytes method for byte size formatting

---
+++ app/Helpers/FormatHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Helpers;
+
+class FormatHelper
+{
+    public static function formatBytes($bytes, $precision = 2): string
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+}
